863d9662116250f7c81135fe164d747895369781
Christian Costa <christiandcosta@gmail.com>
2012-08-14 09:51:10 -0400

    created a user interface to the teams activity. Milestone 0.1

--- CreateTeam/src/com/createteam/Teams.java
+++ CreateTeam/src/com/createteam/Teams.java
@@ -3,6 +3,7 @@ package com.createteam;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.widget.*;
 
 public class Teams extends Activity {
 
@@ -10,6 +11,10 @@ public class Teams extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.teams);
+		
+		//EditText etTeamName = (EditText) findViewById(R.id.etTeamName);
+		//Button bCreatTeam = (Button) findViewById(R.id.bCreateTeam);
+		//ListView lvTeams = (ListView) findViewById(R.id.lvTeams);
     }
 
     @Override
